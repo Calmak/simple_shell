46ead076ea121a64ed10d464ef05414611ed1bd2
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-16 05:26:09 +0200

    functions to implement
    if you have another way of implementing the functions feel free to use that and change all occurrences of the function in the code

--- shell.h
+++ shell.h
@@ -2,5 +2,8 @@
 #define SHELL_H
 
 int _putchar(char c);
-
+char *get_user_input(void);
+char **tokenise(char *input);
+char *getcommand(char **command);
+void execute_cmd(char *cmd);
 #endif /* _SHELL*/
