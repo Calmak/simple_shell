a399fcb1f003e2802b26c3e44833aadebe50bdf9
Kevin Chiwakaya <kchiwakaya@gmail.com>
2023-05-15 20:19:29 -0700

    main function

---
+++ shell_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "shell.h"
+/**
+*main - runs the main loop of the shell
+*@argc: number of arguments passed
+*@argv: arguments passed to main
+*@envp: environment variables
+*Returns: Always 0
+*/
+int main(int argc, char *argv[], char *envp[])
+{
+    while (1)
+    {
+        _putchar('$');
+        char *input = get_user_input();
+        char **string_tokens = tokenise(input);
+        char *cmd = getcommand(string_tokens);
+        excecute_cmd(cmd);
+    }
+    return (0);
+}
