bf69664349a0201194cb49c3183b19ec8aad0270
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-20 09:45:20 +0200

    execute without builtins

--- execute.c
+++ execute.c
@@ -3,5 +3,26 @@
 
 void execute_cmd(char **cmd)
 {
-	
+	int status;
+        pid_t pid = fork(); /**Launch a new process*/
+        
+        if (pid == 0)/**child process*/
+        {
+            execvp(cmd[0],cmd);/**run command*/
+            perror("shell error");
+            exit(1);
+        }
+        else if (pid > 0) /**parent process*/
+        {
+            do
+            {
+                /**wait for child process to exit*/
+                waitpid(pid,&status,WUNTRACED);
+            }
+            while(!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+        else
+        {
+            perror("shell error");
+        }
 }
