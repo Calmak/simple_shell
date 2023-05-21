42913b6e27be73cf2098f959610827356813e9c4
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-21 23:03:33 +0200

    Update shellenv.c

--- shellenv.c
+++ shellenv.c
@@ -4,11 +4,20 @@
  * shell_env - execute env
  * @cmd: pointer to string env
  */
-void shell_env(char **cmd)
+void shell_env(char **cmd, int argc, char **envp)
 {
+	envp = environ;
+	argc = 0;
 	/**To do*/
 	if (strcmp(cmd[0],"env") == 0)
 	{
-		exit(0);
+  		while (*envp != NULL)
+		{
+    			_putchar('$');
+    			_putchar(' ');
+    			puts(*envp);
+    			_putchar('\n');
+    			envp += 2;
+  		}
 	}
 }
