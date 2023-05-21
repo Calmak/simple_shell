a16584f443fc1d02ec0996052299cb49f3709b46
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-21 23:17:37 +0200

    Update shellcd.c

--- shellcd.c
+++ shellcd.c
@@ -4,11 +4,14 @@
  * shell_cd - executes th cd cmd
  * @cmd:pointer to string cd
  * */
-void shell_cd(char **cmd)
+void shell_cd(char **cmd, int argc, char **envp)
 {
+	envp = NULL;
 	/**to do*/
 	if (strcmp(cmd[0],"cd") == 0)
 	{
-		exit(0);
+		 if (argc == 1)
+			 chdir(getenv("HOME"));
+		chdir(cmd[1]);
 	}
 }
