077ee74cfd0a34fed454d76413eb4b65e9b52dcc
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-21 10:39:06 +0200

    implement structures and function pointers

--- execute.c
+++ execute.c
@@ -5,50 +5,39 @@
 void execute_cmd(char **cmd)
 {
 	int status;
-        pid_t pid; 
+    pid_t pid; 
 	int num_cmd = 0;
 	int i;
+	size_t size;
+	_function _functions[] = {{"cd",shell_cd},{"exit",shell_exit},{"env",shell_env}};
 	
-	if (strcmp(cmd[0], "cd") == 0) 
-	{
-		/**change directory*/
-    		chdir(cmd[1]);
-  	}
-	else if (strcmp(cmd[0], "exit") == 0)
-	{
-    		/** Exit the shell.*/
-    		exit(0);
-  	}
-	else if (strcmp(cmd[0], "env") == 0) 
-	{
-    		/**Print the environment variables.*/
-    		for (i = 0; i < num_cmd; i++)
-		{
-      			printf("%s=%s\n", cmd[i], getenv(cmd[i]));
-    		}
-	}
-	else
-	{
-	
-		pid = fork(); /**Launch a child process*/
-		if (pid == 0)/**child process*/
-        	{
-          		execvp(cmd[0],cmd);/**run command*/
-            		perror("shell error");
-            		exit(1);
-        	}
-        	else if (pid > 0) /**parent process*/
-        	{
-         	do
-            	{
-               		/**wait for child process to exit*/
-               		waitpid(pid,&status,WUNTRACED);
-            	}
-            	while(!WIFEXITED(status) && !WIFSIGNALED(status));
-        	}
-       		else
-        	{
-            		perror("shell error");
-        	}
-	}
+	size = array_length(_functions);
+	for (i = 0; i < size;i++)
+    {
+        if (strcmp(_functions[i],cmd[0])==0)
+        {
+            _functions[i]._function(cmd);
+            return;
+        }
+    }
+	pid = fork(); /**Launch a child process*/
+    if (pid == 0)/**child process*/
+    {
+        execvp(cmd[0],cmd);/**run command*/
+        perror("shell error");
+        exit(1);
+    }
+    else if (pid > 0) /**parent process*/
+    {
+        do
+        {
+            /**wait for child process to exit*/
+            waitpid(pid,&status,WUNTRACED);
+        }
+        while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    else
+    {
+        perror("shell error");
+    }
 }
