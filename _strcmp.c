2fec4bc8660b8ac8f1146d12471c7fd91b95e5dc
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-23 13:57:41 +0200

    Update _strcmp.c

--- _strcmp.c
+++ _strcmp.c
@@ -1,10 +1,16 @@
 #include "shell.h"
+/**
+* _strcmp - function to compare 2 strings
+* @string1: first string
+* @string2: second string
+*Returns: O if same 
+*/
 
 int _strcmp(const char *string1, const char *string2) 
 {
   while (*string1 == *string2) 
   {
-    if (*string1 == '\0') 
+    if (*string1 == '\0') /**End of string*/
     {
       	return 0;
     }
