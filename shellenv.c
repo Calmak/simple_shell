353ad2ce286104b07f5a3eb116572dba74530713
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-23 09:47:51 +0200

    Update shellenv.c

--- shellenv.c
+++ shellenv.c
@@ -10,24 +10,9 @@
 void shell_env(char **cmd, int argc __attribute__((unused)), char **envp)
 {
 	int i;
-  	int num = 0;
-
-  	while (envp[num] != NULL) {
-   		num++;
- 	 }
-	envp = malloc(sizeof(**envp * num));
-
- 	 if (strcmp(cmd[0], "env") == 0)
+ 	if (strcmp(cmd[0], "env") == 0)
  	 {
-   	 	for (i = 0; i < num; i++) 
-		{
-      			putchar('$');
-      			while (*envp[i] != '\0' && *envp[i] != '=')
-			{
-        			putchar(*envp[i++]);
-      			}
-      		putchar('\n');
+   	 	for (i = 0 ; envp[i] ; i++) 
+			printf( "envp[%d]: %s\n", i, envp[i] );
     	}
-  }
-free(envp);
 }
