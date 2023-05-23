c0ef266b2ef12c15929b0c05d0faedeb73bee639
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-23 08:01:31 +0200

    Update shellenv.c

--- shellenv.c
+++ shellenv.c
@@ -12,11 +12,11 @@ void shell_env(char **cmd, int argc __attribute__((unused)), char **envp)
 	
 	if (strcmp(cmd[0],"env") == 0)
 	{
-		while (*env != NULL)
+		while (*envp != NULL)
 		{
     			putchar('$');
-    			while (*env != '\0' && *env != '=')
-      				putchar(*env++);
+    			while (*envp != '\0' && *envp != '=')
+      				putchar(*envp++);
    			 putchar('\n');
   		}
 	}
