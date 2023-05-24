1b8e52bb9c8808db1eddf0c06f580d711c079b5b
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-24 09:12:04 +0200

    Update path.c

--- path.c
+++ path.c
@@ -2,38 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int find_cmd(char *cmd, char **argv)
-{
+int find_cmd(char *cmd, char **argv) {
   char *path = getenv("PATH");
   int is_found = 0;
   char *dir;
   char *full_path;
-  int i = 0;
 
-  if (path == NULL) 
-  {
+  if (path == NULL) {
     perror(argv[0]);
     return -1;
   }
-  while (path)
-  {
-      dir = path[i];
-      full_path = malloc(strlen(dir) + strlen(cmd) + 2);
-      strcpy(full_path, dir);
-      strcat(full_path, "/");
-      strcat(full_path, cmd);
 
-      if (access(full_path, X_OK) == 0) 
-      {
-        is_found = 1;
-        break;
-      }
+  while (path != NULL) {
+    dir = strtok(path, ":");
+    full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+    strcpy(full_path, dir);
+    strcat(full_path, "/");
+    strcat(full_path, cmd);
 
-    path++;
-    i++;
+    if (access(full_path, X_OK) == 0) {
+      is_found = 1;
+      break;
     }
+
     free(full_path);
+    path = strtok(NULL, ":");
+  }
 
-    return is_found;
+  return is_found;
 }
-
