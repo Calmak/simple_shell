c0d23cbdaf7f88382f716c1dd6609f4dccc31331
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-24 08:24:01 +0200

    Update path.c

--- path.c
+++ path.c
@@ -1,28 +1,33 @@
-int find_cmd(char *cmd) {
-  char *path = getenv("PATH");
-  char *token;
+#include "shell.h"
+#include <stdlib.h>
+#include <string.h>
 
-  if (path == NULL) {
-    printf("Error: PATH environment variable is not set\n");
+int find_cmd(char *cmd, char **argv)
+{
+  char *path = getenv("PATH");
+   int is_found = 0;
+
+  if (path == NULL) 
+  {
+    perror(argv[0]);
     return -1;
   }
-
-  int found = 0;
-
-  for (char *dir in path) {
+  for (char *dir in path)
+  {
     char *full_path = malloc(strlen(dir) + strlen(cmd) + 2);
     strcpy(full_path, dir);
     strcat(full_path, "/");
     strcat(full_path, cmd);
 
-    if (access(full_path, X_OK) == 0) {
-      found = 1;
+    if (access(full_path, X_OK) == 0) 
+    {
+      is_found = 1;
       break;
     }
 
     free(full_path);
   }
 
-  return found;
+  return is_found;
 }
 
