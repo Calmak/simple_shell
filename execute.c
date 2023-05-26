634268cbe4ec580b8be4636f2ba863bf8b96417e
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-05-26 12:23:33 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -18,12 +18,12 @@ void execute_cmd(char **cmd, int argc, char **argv, char **envp)
 	_function _functions[] = {{"cd", shell_cd},
 		{"exit", shell_exit}, {"env", shell_env}};
 	size = sizeof(_functions) / sizeof(_function);
-	avail = find_cmd(cmd[0], argv);
+	/*avail = find_cmd(cmd[0], argv);
 	if (avail)
 	{
 		perror(argv[0]);
 		return;
-	}
+	}*/
 	for (i = 0; i < size; i++)
 	{
 		if (_strcmp(_functions[i].name, cmd[0]) == 0)
